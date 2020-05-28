b1012fe32ff7dbd11f80949933c494377c80742e
Daniel Santosa <19315466@student.uwa.edu.au>
2020-05-28 16:39:50 +0800

    Update comments

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+# add flask starting module
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
